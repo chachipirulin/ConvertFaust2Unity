633e380da015c305448146ecc57288e126266429
chachipirulin <62192287+chachipirulin@users.noreply.github.com>
2021-02-06 14:31:37 +0100

    Files to use in the video example

---
+++ FaustVariableController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaustVariableController : MonoBehaviour
+{
+    public FaustPlugin_freeverbPlugin scriptFaust;
+    public int dummy_Toggle = 1;
+    public GameObject this_Object;
+    public float wet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("FAUSTVARIABLECONTROLLER: inside Start");
+        scriptFaust = this_Object.GetComponent<FaustPlugin_freeverbPlugin>();
+        wet = scriptFaust.getParameter(3);
+        print("scriptFaust.parameters damp = " + wet);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (dummy_Toggle == 1)
+        {
+            // setParameter(int param, float x)
+            scriptFaust.setParameter(3, 0.9f);
+            print("VALUE CHANGED! scriptFaust.getParameter(3)= " + (scriptFaust.getParameter(3)));
+            dummy_Toggle = 0;
+        }
+    }
+}
